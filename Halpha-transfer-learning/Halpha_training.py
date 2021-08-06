8f1b17913acedaa112effec877b0bb857d134742
elentirme <88392011+elentirme@users.noreply.github.com>
2021-08-06 12:41:20 -0500

    Update Halpha_training.py

--- Halpha-transfer-learning/Halpha_training.py
+++ Halpha-transfer-learning/Halpha_training.py
@@ -42,7 +42,7 @@ class HalphaDataset(mrcnn.utils.Dataset):
             row_s, row_e = box[1], box[3]
             col_s, col_e = box[0], box[2]
             masks[row_s:row_e, col_s:col_e, i] = 1
-            class_ids.append(self.class_names.index('kangaroo'))
+            class_ids.append(self.class_names.index('Halpha'))
         return masks, asarray(class_ids, dtype='int32')
 
     # A helper method to extract the bounding boxes from the annotation file
